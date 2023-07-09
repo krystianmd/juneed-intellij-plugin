13229232ef65bd8570cbc97cb527bf039739b401
kmadra <madrakrystian@gmail.com>
2023-07-09 13:13:53 +0200

    Reformat plugin language configuration

--- src/main/kotlin/com/madrakrystian/juneed/PluginLanguageConfiguration.kt
+++ src/main/kotlin/com/madrakrystian/juneed/PluginLanguageConfiguration.kt
@@ -8,11 +8,24 @@ import org.jetbrains.kotlin.idea.KotlinLanguage
 private const val JAVA_TEMPLATE_CONTEXT_ID = "JAVA_TEST"
 private const val KOTLIN_TEMPLATE_CONTEXT_ID = "KOTLIN_TEST"
 
-enum class PluginLanguageConfiguration(val language: Language, val templateContext: TemplateContext) {
-    JAVA(JavaLanguage.INSTANCE,
-            TemplateContext(id = JAVA_TEMPLATE_CONTEXT_ID, presentableName = ResourceBundle.message("template.context.name.java"))),
-    KOTLIN(KotlinLanguage.INSTANCE,
-            TemplateContext(id = KOTLIN_TEMPLATE_CONTEXT_ID, presentableName = ResourceBundle.message("template.context.name.kotlin")));
+enum class PluginLanguageConfiguration(
+    val language: Language,
+    val templateContext: TemplateContext,
+) {
+    JAVA(
+        JavaLanguage.INSTANCE,
+        TemplateContext(
+            id = JAVA_TEMPLATE_CONTEXT_ID,
+            presentableName = ResourceBundle.message("template.context.name.java")
+        )
+    ),
+    KOTLIN(
+        KotlinLanguage.INSTANCE,
+        TemplateContext(
+            id = KOTLIN_TEMPLATE_CONTEXT_ID,
+            presentableName = ResourceBundle.message("template.context.name.kotlin")
+        )
+    );
 
     data class TemplateContext(val id: String, val presentableName: String)
 }
